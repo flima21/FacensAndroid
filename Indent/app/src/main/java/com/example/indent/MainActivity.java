fd589b7668c452fe1c2b992e61c891f264400ac3
Felipe Pedroso de Lima <felipe.pedroso2108@gmail.com>
2024-03-18 21:54:56 -0300

    18-03 Criação de eventos para ativar apps no android

--- Indent/app/src/main/java/com/example/indent/MainActivity.java
+++ Indent/app/src/main/java/com/example/indent/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.indent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PackageManagerCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button buttonEnviaMensagem;
+    Button buttonLigacao;
+    Button buttonMapa;
+    Button buttonSite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Recupera o botão
         buttonEnviaMensagem = (Button) findViewById(R.id.buttonTelaMensagem);
+        buttonLigacao = (Button) findViewById(R.id.buttonLigacao);
+        buttonMapa = (Button) findViewById(R.id.buttonMapa);
+        buttonSite = (Button) findViewById(R.id.buttonSite);
         // Sobrescrita do método click do botão
         buttonEnviaMensagem.setOnClickListener(v -> {
             Intent intent = new Intent(this, TelaMensagemActivity.class);
@@ -22,10 +35,40 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        // Botão para fazer ligação
+        buttonLigacao.setOnClickListener(v -> {
+            Uri uri = Uri.parse("tel: " + "9999999");
+            Intent intent = new Intent(Intent.ACTION_DIAL,uri);
+
+            int permissionCheck = ContextCompat.checkSelfPermission(this,android.Manifest.permission.CALL_PHONE);
+
+            if(this.verifyPermission(permissionCheck)) {
+                startActivity(intent);
+            }
+            else {
+                ActivityCompat.requestPermissions(this, new String[]{ android.Manifest.permission.CALL_PHONE },1);
+            }
+        });
+
+        buttonSite.setOnClickListener(v -> {
+            Uri uri = Uri.parse("https://facens.br");
+            Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+            int permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.INTERNET);
+
+            if(this.verifyPermission(permissionCheck)) startActivity(intent);
+            else {
+                ActivityCompat.requestPermissions(this, new String[]{ android.Manifest.permission.INTERNET },1);
+            }
+        });
+
+
 
     }
 
 
+    private boolean verifyPermission(int permission) {
+        return permission == PackageManager.PERMISSION_GRANTED;
+    }
 
 
 }
